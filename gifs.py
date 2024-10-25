882cd2e521488883bbb7154a20cde6d973a467b1
Brian Chen <garbagebindev@gmail.com>
2024-10-25 14:24:44 -0700

    Update gifs.py

--- gifs.py
+++ gifs.py
@@ -1,24 +1,6 @@
 import random
 
 class GIF:
-    gifs = [
-        "https://tenor.com/view/dashspider-dash-geometrydash-geometry-dash-colon-gif-6183807318430948778",
-        "https://media.discordapp.net/attachments/1193274419284561931/1205099972698308638/pou.gif?ex=66c714cf&is=66c5c34f&hm=53f151686f818a57069b6987209070c0bae70bb7fab8bcd1238a862c729a17bf&",
-        "https://media.discordapp.net/attachments/744329506827010152/1181678153446461511/attachment.gif?ex=66c6e84a&is=66c596ca&hm=b3c1986ae8a248a09553fc5875dfd842e4f8e2da61dc827947fb4d606c995974&",
-        "https://media.discordapp.net/attachments/1073842702346616886/1197757147526742118/1473EA6E-8A49-4638-B6A5-8FD7953A8EBB.gif?ex=66c6bc45&is=66c56ac5&hm=4df8224e14a772a49f31b2fc3519d84e45c2e9a6a55712cd26031d9d6b44e507&",
-        "https://cdn.discordapp.com/attachments/422448194476048403/914011443513401374/you_be_like-1.gif?ex=66c6bffd&is=66c56e7d&hm=c76af145424b38f1af195c3e6c55bf04730e3d2022ef28260ca144e339652806&",
-        "https://media.discordapp.net/attachments/900617920126783502/1008389949223079956/attachment.gif?ex=66c752d2&is=66c60152&hm=168544adef33480982147e957565ee81a451133e9f671f9d9051918495373232&",
-        "https://media.discordapp.net/attachments/745707366133006348/965773091600990208/sendo.gif?ex=66c73108&is=66c5df88&hm=56faf9a8b192e60449f080f5a6f7f376784ccc90bbe5e0ed75bb7ad84d03232a&",
-        "https://tenor.com/view/lil-nas-x-pregnant-discord-gif-24975919",
-        "https://media.discordapp.net/attachments/897319839251693618/996873887121948702/attachment-1-1.gif?ex=66c6f4e7&is=66c5a367&hm=4c523ecec803c3543b54fccfa219d6dc2b955de7b9852834d774548e5bfd3461&",
-        "https://cdn.discordapp.com/attachments/1177043753333178412/1209242417514020884/qnWDNLIw.gif?ex=66dcbe42&is=66db6cc2&hm=8936aca27db8b5d93e2f98848bfbb382cfaa6b5f5c6c026b892d3739b2c8da87&",
-        "https://tenor.com/view/freaky-dog-im-fr-an-eater-qazira-dog-tongue-freaky-dog-tongue-gif-14576727459878736156",
-        "https://media.discordapp.net/attachments/918580732807499799/1086786359160737803/FqqfMeBXsAIBBpt.gif?ex=66dcdb2b&is=66db89ab&hm=4fc2da30528b83d85dbd73cdbed93113d6ed4fc7a4aded52e2fa6d770c0b724e&",
-        "https://tenor.com/view/skibi-di-skibidi-toilet-skibidi-cameraman-skibidi-shower-gif-14100519840818449994",
-        "https://uploads.disquscdn.com/images/fa1bf4a66d4dcdc0b6f68630ba4b4223ee4e99e8d18d0025f468bad618b84b28.gif",
-        "https://tenor.com/view/omg-what-no-way-emoji-shock-gif-24390671",
-        "https://media.discordapp.net/attachments/940294974539448360/1109919515556450445/attachment.gif?ex=66dca399&is=66db5219&hm=4ff675c07aa00983d10cf7072db30aee01d2f8f22b8e596290508b828d89f214&",
-        "https://cdn.discordapp.com/attachments/1084927327277957171/1089260902005620827/attachment.gif?ex=66dca144&is=66db4fc4&hm=ad4c0804561161b53d326b4527a5ed3735ed7d9f9fced05a7e0edfe085338294&",
-    ]
+    gifs = []
 
 
